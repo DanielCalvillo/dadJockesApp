2a3a7576f1579444296f62370e7216e4b148a7b9
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-03-28 14:07:07 -0600

    Styling the JokeList Component

--- src/Components/JockeList.jsx
+++ src/Components/JockeList.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react' 
+import React, { Component } from 'react'
+import '../Assets/JockeList.css' 
 import JockeLine from './JockeLine';
 import axios from 'axios'
 
@@ -27,17 +28,22 @@ class JockeList extends Component {
         }
 
         this.setState(st => ({
-            jockes: [...st.jockes, jokes]
+            jockes: jokes
         }))
     }
 
     render() {
 
         return(
-            <div>
-                <div className="JokeList">
-                    <h1>Dad Jokes</h1>
+            <div className="JokeList">
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title">
+                        <span>Dad</span> Jokes
+                    </h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                    <button className='jockeList-getmore'>New Jockes</button>
                 </div>
+
                 <div className="JokeList-jokes">
                     {this.state.jockes.map( j => (
                         <div>{j}</div>
