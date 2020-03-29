8267ec4c8b984735df3c9bdb398dd92296287844
Daniel Calvillo <danielcalvillomancilla123@gmail.com>
2020-03-28 18:59:26 -0600

    Final touches in animation

--- src/Components/JockeList.jsx
+++ src/Components/JockeList.jsx
@@ -95,7 +95,7 @@ class JockeList extends Component {
                         <span>Dad</span> Jokes
                     </h1>
                     <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-                    <button className='jockeList-getmore' onClick={this.handleClick}>New Jokes</button>
+                    <button className='JokeList-getmore' onClick={this.handleClick}>New Jokes</button>
                 </div>
 
                 <div className="JokeList-jokes">
